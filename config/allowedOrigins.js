442ed553d0177748d3f9b4e4f9a72a4d4911600e
manish2-0 <maishkumawat12955@gmail.com>
2023-04-18 15:40:18 +0530

    Added allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
     'http://localhost:8000',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://blp-attendance-backend.vercel.app',
+    'https://blp-attendance.vercel.app'
 ];
 
 module.exports = allowedOrigins;
