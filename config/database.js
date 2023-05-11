f01ece9075d5b32046ca480a549e157a22d2767c
manish2-0 <maishkumawat12955@gmail.com>
2023-05-11 23:13:55 +0530

    changes

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USERNAME,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_DBNAME
-  
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "attendance"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME
+
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "attendance"
 })
 
 connection.connect();
