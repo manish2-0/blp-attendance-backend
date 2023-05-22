05c0e10991851b90a3484a87227f9e9c891419e7
manish2-0 <maishkumawat12955@gmail.com>
2023-05-22 21:42:25 +0530

    minor

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DBNAME
+   database: process.env.DB_DBNAME
 
   // host: "localhost",
   // user: "root",
