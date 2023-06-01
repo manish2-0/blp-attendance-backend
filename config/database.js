63889e987fa836404c9d615739fcc9edf2be5e52
manish2-0 <maishkumawat12955@gmail.com>
2023-06-01 21:34:16 +0530

    Tp

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
+   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DBNAME
